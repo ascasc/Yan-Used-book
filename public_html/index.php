329c013ed9c5953a8a30d842c196b804b019f778
顏章羽 <asc824@gmail.com>
2019-08-20 08:29:46 +0800

    切完購物清單的版以及寫入開啟與關閉的JS，並且註解HTML.CSS.JS，另外修改HTML的視窗為ID

--- public_html/index.php
+++ public_html/index.php
@@ -4,16 +4,19 @@
     <div class="logo">Y</div>
     <nav class="clearfix">
       <ul class="nav">
-        <!-- 此為template產生 -->
+        <!-- 此為navTemplate產生 -->
       </ul>
     </nav>
-    <div class="shopcart-Panel">
+    <!-- 此為購物車 -->
+    <div id="shopcart-Panel">
       <p>你的購物車是空的</p>
+      <!-- 此為購物車Template部分產生 -->
       <div class="button">結帳</div>
     </div>
-    <div class="Menu-Panel">
+    <!-- 此為選單 -->
+    <div id="Menu-Panel"> 
       <li class="update-member-nav">修改會員資料</li>
-      <li>購物清單</li>
+      <li class="shopping-list-nav">購物清單</li>
       <li>登出</li>
     </div>
   </div>
@@ -22,6 +25,7 @@
     <img src="img/bg.jpeg" alt="bg.jpeg">
     <h1>歡迎來到 Yan二手書店</h1>
   </div>
+  <!-- 此為登入系統 -->
   <div id="login" class="login-signup">
     <form>
       <div class="close">x</div>
@@ -37,6 +41,7 @@
       <center><a href="#">忘記密碼?</a></center>
     </form>
   </div>
+  <!-- 此為註冊資料 -->
   <div id="signup" class="login-signup">
     <form>
       <div class="close">x</div>
@@ -57,7 +62,7 @@
       <div class="button">註冊</div>
     </form>
   </div>
-
+  <!-- 此為修改個人資料 -->
   <div id="update-member" class="login-signup">
     <form>
       <div class="close">x</div>
@@ -77,6 +82,14 @@
       <div class="button">修改</div>
     </form>
   </div>
+  <!-- 此為購物清單 -->
+  <div id="shopping-list">
+    <div class="close">x</div>
+    <h2>購物清單</h1>
+      <!-- 此為購物清單Template部分產生 -->
+      
+  </div>
+  <!-- 此為狀態訊息 -->
   <div id="signu-success">
     <p>註冊成功</p>
     <div class="button close">確定</div>
@@ -103,5 +116,14 @@
     <div class="delete">刪除</div>
   </li>
 </script>
-
+<!-- 購物清單Template部分 -->
+<script id="shopping-list-item-template" type="text/x-handlebars-template"> 
+ <li>
+ <!-- 分待付款.待出貨.待收貨.已完成 -->
+    <em>待付款</em>  
+    <img src="http://lorempixel.com/50/50/" alt="">
+    <div class="content">現代PHP新的特點及良好的習慣</div>
+    <div class="price"><span>1</span>NT$<span>580</span></div>
+  </li>
+</script>
 <?php include('footer.php'); ?>
--- public_html/js/action.js
+++ public_html/js/action.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
-  
+  //handlebard最初的準備時候
   var nav_template = $('#nav-item-template').html();
   var nav_handlebars =Handlebars.compile(nav_template);
 
   var panel={
     el: '#panel',
   };
-  var login={
+  // 登入視窗
+  var login={ 
     el: '#login',
   };
+  // 註冊
   var signup={
     el: '#signup',
   };
+  // 狀態訊息視窗
   var signu_success={
     el: '#signu-success',
   };
+  // 修改個人資料視窗
   var update_member={
     el:'#update-member',
   };
+  // 購物清單視窗
+  var shopping_list ={
+    el: '#shopping-list',
+  }
+  // 選單視窗
+  var Menu_Panel ={
+    el: '#Menu-Panel',
+  }
+  // 購物車視窗
+  var shopcart_Panel={
+    el: '#shopcart-Panel',
+  }
+  //主要為Template物件使用
   var template ={
     nav:function(li_1, li_val_1, li_2_1, li_val_2, li_2_2){
         var login_nav_item={
@@ -31,7 +48,8 @@ $(document).ready(function(){
       $(panel.el).find('#header nav .nav').prepend(login_nav_complile);
     },
   };
-  var public_signup_login={//登入與註冊訊息
+  // 主要為登入與註冊訊息物件使用
+  var public_signup_login={
     alert_msg: function(el,alert_msg) {
       $('.error-msg').addClass('open');
       $(el).find('.error-msg .alert-danger').text(alert_msg);
@@ -45,31 +63,40 @@ $(document).ready(function(){
       $(signu_success.el).addClass('open');
     } 
   };
-
+  // 為登入的nav
   template.nav('login-nav','登入','signup-nav', '註冊');
     $(panel.el)
       .on('click', '.login-nav', function(e){//開啟登入視窗
+        e.preventDefault();
         $('#login').addClass('open');
     })
       .on('click', '.signup-nav', function(e){//開啟註冊視窗
+        e.preventDefault();
         $(signup.el).addClass('open');
     })
       .on('click', '.update-member-nav', function(e){//登入後的修改資料
+        e.preventDefault();
         $(update_member.el).addClass('open');
+    })
+      .on('click', '.shopping-list-nav', function(e) {//開啟購物清單
+        e.preventDefault();
+        $(shopping_list.el).addClass('open');
     })
       .on('click', '.dropdown', function(e) {//呼叫選單
       e.preventDefault();
-      $(".Menu-Panel").slideToggle(200);
+      $(Menu_Panel.el).slideToggle(200);
     }) 
       .on('click', '.shop-cart-nav', function(e) {//呼叫購物車
       e.preventDefault();
-      $(".shopcart-Panel").slideToggle(200);
+      $(shopcart_Panel.el).slideToggle(200);
     })
       .on('click', '.close', function(e){   //關閉視窗處理
+        e.preventDefault();
         $(signup.el).removeClass('open');  
         $('#login').removeClass('open');
         $(signu_success.el).removeClass('open');
         $(update_member.el).removeClass('open');
+        $(shopping_list.el).removeClass('open');
         $(this).siblings('input').val('');
         public_signup_login.alert_msg_off();
     })
